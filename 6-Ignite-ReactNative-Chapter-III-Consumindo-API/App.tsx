cfc36e6d43e2893b4c371ab09e1fa01ff0d7db6e
douglasneves-net <d88.neves@gmail.com>
2022-05-19 14:47:33 -0300

    feat: main theme definition
    main theme definition

--- 6-Ignite-ReactNative-Chapter-III-Consumindo-API/App.tsx
+++ 6-Ignite-ReactNative-Chapter-III-Consumindo-API/App.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
+
 import { Home } from './src/screens/Home';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+
   return <Home/>
 }
 
