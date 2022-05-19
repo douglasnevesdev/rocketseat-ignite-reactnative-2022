c6a3e7c543af6c1c1be71f5be665d1ccf4fb1394
douglasneves-net <d88.neves@gmail.com>
2022-05-19 15:48:49 -0300

    feat: Add Images and Add Type .svg and .png
    Add Images and Add Type .svg and .png

--- 6-Ignite-ReactNative-Chapter-III-Consumindo-API/src/screens/Home/index.tsx
+++ 6-Ignite-ReactNative-Chapter-III-Consumindo-API/src/screens/Home/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import Logo from '../../assets/logo.svg';
 
 import {
  Container,
- Title
+ Header
 } from './styles';
 
 export function Home(){
   return (
      <Container>
-       <Title>Home</Title>
+       <StatusBar barStyle="light-content" backgroundColor='transparent' translucent/>
+       <Header>
+        <Logo/>
+       </Header> 
      </Container>
   );
 }
--- 6-Ignite-ReactNative-Chapter-III-Consumindo-API/src/screens/Home/styles.ts
+++ 6-Ignite-ReactNative-Chapter-III-Consumindo-API/src/screens/Home/styles.ts
@@ -2,12 +2,11 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center ;
-  justify-content: center ;
   background-color: ${({theme}) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-  font-size: 30px;
-  font-family: ${({ theme }) => theme.fonts.primary_400} ;
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+  background-color: ${({theme}) => theme.colors.header};
 `;
