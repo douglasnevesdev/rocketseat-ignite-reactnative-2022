cdbecdd4775fbe261937b1fe66252f004df606c7
douglasneves-net <d88.neves@gmail.com>
2022-05-22 17:39:02 -0300

    feat: Add component Car
     Add component Car

--- 6-Ignite-ReactNative-Chapter-III-Consumindo-API/src/screens/Home/index.tsx
+++ 6-Ignite-ReactNative-Chapter-III-Consumindo-API/src/screens/Home/index.tsx
@@ -1,30 +1,57 @@
 import React from 'react';
-import { RFValue } from 'react-native-responsive-fontsize'; 
+import { RFValue } from 'react-native-responsive-fontsize';
 import { StatusBar } from 'react-native';
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import {
- Container,
- Header,
- TotalCars,
- HeaderContent
+  Container,
+  Header,
+  TotalCars,
+  HeaderContent
 } from './styles';
 
-export function Home(){
+export function Home() {
+
+  const carDataOne = {
+    brand: "Audi",
+    name: "RS 5 Coupé",
+    rent: {
+      period: "AO DIA",
+      price: 120,
+    },
+    thumbnail: "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png"
+  }
+
+  const carDataTwo = {
+    brand: "Porsche",
+    name: "Panamera",
+    rent: {
+      period: "AO DIA",
+      price: 340,
+    },
+    thumbnail: "https://purepng.com/public/uploads/large/purepng.com-porsche-panamera-white-carcarvehicletransportporsche-961524659258pfvb8.png"
+  }
+
   return (
-     <Container>
-       <StatusBar barStyle="light-content" backgroundColor='transparent' translucent/>
-       <Header>
-         <HeaderContent>
-            <Logo 
-              width={RFValue(108)}
-              height={RFValue(12)}
-            />
-            <TotalCars>
-              Total de 12 carros.
-            </TotalCars>
-         </HeaderContent>
-       </Header> 
-     </Container>
+    <Container>
+      <StatusBar barStyle="light-content" backgroundColor='transparent' translucent />
+      <Header>
+        <HeaderContent>
+          <Logo
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total de 12 carros.
+          </TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
+
+
+    </Container>
   );
 }
